dc6e429e46306247b455e7654b4faadb0ae98584
Uyooghene <uyooghene01@gmail.com>
2024-02-25 09:46:05 +0100

    practice functions

---
+++ javascript/gradeapp.js
@@ -0,0 +1,22 @@
+function grade(score) {
+    if(score>=90 && score<=100){
+      return ('A');
+    }
+    if(score>=80 && score<=89){
+      return 'B';
+    }
+    if(score>=70 && score<=79){
+      return'C';
+    }
+    if(score>=60 && score<=69){
+      return 'D';
+    }
+    if(score>=50 && score<=59){
+      return'E';
+    }
+    if(score>=0 && score<=49){
+      return'F';
+    }
+  
+  }
+  
